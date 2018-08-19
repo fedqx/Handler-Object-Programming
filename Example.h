406146ba44c7522264d3cec364f06aa3730e6c26
Fatih Melih Özgün <34399010+fedqx@users.noreply.github.com>
2018-08-19 11:45:16 +0300

    Update Example.h

--- Example.h
+++ Example.h
@@ -1,15 +1,1 @@
-#include <SDL.h>
-#include <SDL_image.h>
-#include <stdbool.h>
-#include "Entity.h"
-struct Graphics
-{
-	SDL_Renderer* sldrenderer;
-	SDL_Window *window;
-	int h;
-	int w;
-	
-};
-bool initg(struct Graphics *g,);
-bool render(struct Entity *e, struct Graphics *g);
-bool loadtexture(struct Entity *e, struct Graphics *g);
+
